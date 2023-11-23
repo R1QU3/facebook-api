8d1a1ee86cb0e58ce03d3aec77ee0b17d4dec355
Jose Henrique <josehenriquehyttspf@gmail.com>
2023-11-23 12:21:05 -0300

    feat(routes): adiciona rotas de autenticacao

--- start/routes/index.ts
+++ start/routes/index.ts
@@ -3,3 +3,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { message: 'The application is already running' }
 })
+
+// Auth
+Route.post('/auth', 'Auth/AuthController.store')
+Route.delete('/auth', 'Auth/AuthController.destroy').middleware('auth')
